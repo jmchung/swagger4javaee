8a9f9df0d20e099901da71c3a4382bc95da372fc
Jen-Ming Chung <jenmingisme@gmail.com>
2014-04-03 04:22:12 +0800

    Remove the resources.add(com.wordnik.swagger.jaxrs.JaxrsApiReader.class); from ApplicationConfig to avoid error warning in Glassfish.

--- swagger4javaee-web/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ swagger4javaee-web/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.netbeans.rest.application.config;
 
 import java.util.Set;
@@ -19,8 +18,18 @@ public class ApplicationConfig extends Application {
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new java.util.HashSet<>();
-        resources.add(com.wordnik.swagger.jaxrs.JaxrsApiReader.class);
-        resources.add(com.wordnik.swagger.jaxrs.listing.ApiListingResource.class);        
+        /**
+         * Removing it removes the warning and Swagger still works as expected.
+         *
+         * resources.add(com.wordnik.swagger.jaxrs.JaxrsApiReader.class);
+         *
+         * WARNING: A provider com.wordnik.swagger.jaxrs.JaxrsApiReader
+         * registered in SERVER runtime does not implement any provider
+         * interfaces applicable in the SERVER runtime. Due to constraint
+         * configuration problems the provider
+         * com.wordnik.swagger.jaxrs.JaxrsApiReader will be ignored.
+         */
+        resources.add(com.wordnik.swagger.jaxrs.listing.ApiListingResource.class);
         resources.add(com.wordnik.swagger.jaxrs.listing.ApiDeclarationProvider.class);
         resources.add(com.wordnik.swagger.jaxrs.listing.ApiListingResourceJSON.class);
         resources.add(com.wordnik.swagger.jaxrs.listing.ResourceListingProvider.class);
@@ -29,13 +38,12 @@ public class ApplicationConfig extends Application {
     }
 
     /**
-     * Do not modify addRestResourceClasses() method.
-     * It is automatically populated with
-     * all resources defined in the project.
-     * If required, comment out calling this method in getClasses().
+     * Do not modify addRestResourceClasses() method. It is automatically
+     * populated with all resources defined in the project. If required, comment
+     * out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(com.example.rest.hello.HelloResource.class);
     }
-    
+
 }
